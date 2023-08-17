6e3f32e598b10d47d566a54e7335799268112201
Geek_Tech <geecktech.och2@gmail.com>
2023-08-17 16:39:12 +0600

    'lesson4'

--- lessons/lesson3.py
+++ lessons/lesson3.py
@@ -1,4 +1,7 @@
 # инкапсуляция
+
+
+
 # 1 капсула
 # 2 уровни защиты
 # 3 публичный,приватный,скрытый
@@ -10,6 +13,15 @@ class Bank:
         self._money = money
         self.__passw = password
 
+    def setpass(self, n):
+        if len(self.__passw) < 10:
+            print('слишком маленький ')
+        else:
+            self.__passw = n
+
+    def getpass(self):
+        print(self.__passw)
+
     def pname(self):
         print(self.name, self._money)
 
@@ -19,13 +31,19 @@ class Bank:
     def pasww(self):
         self.__ppas()
 
+
 beka = Bank('бека', 20, 0, '12345678987543')
-beka._money = 123456789
-
-print(beka._money)
-beka.pname()
-
-beka.__passw = '0'
-print(beka.__passw)
-beka.pasww()
-print(dir(Bank))
+beka.getpass()
+beka.setpass('qwerdwvfsd')
+beka.getpass()
+# beka._money = 123456789
+#
+# print(beka._money)
+# beka.pname()
+#
+# beka.__passw = '0'
+# print(beka.__passw)
+# beka._Bank__passw = '23'
+# beka.pasww()
+# beka.pasww()
+print(dir(beka))
---
+++ lessons/lesson4.py
@@ -0,0 +1,34 @@
+# множественное наследование
+# MRO - method resolution order порядок выполнения методов
+
+class A:
+    def __init__(self, a):
+        self.a = a
+
+    def c(self):
+        print(self,'это метод А')
+
+
+class A2(A):...
+
+
+class C:
+    def __init__(self, с):
+        self.с = с
+
+    def c1(self):
+        print(self,'это метод C')
+
+
+
+class B(A2,C,A):
+    def __init__(self,a,c):
+        A.__init__(self,a)
+        C.__init__(self,c)
+
+
+
+
+b=B('a','c')
+b.c()
+print(B.mro())
